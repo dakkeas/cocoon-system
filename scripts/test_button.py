b51ebfc52243abdc0c4a58b63d91be47e24be2dc
Justine Rey Daquis <justinedaquis28@gmail.com>
2026-02-20 18:16:04 +0800

    toggle button to push button

--- scripts/test_button.py
+++ scripts/test_button.py
@@ -1,25 +1,35 @@
 
-
 import RPi.GPIO as GPIO
 import time
 import os
 import config
 
-BUTTON_PIN = config.BUTTON_PIN
+BUTTON_PIN = config.START_BUTTON_PIN
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-print("Waiting for button press...")
+button_already_pressed = False
+
+def button_pressed(channel):
+    global button_already_pressed
+    
+    if GPIO.input(BUTTON_PIN) == GPIO.LOW and not button_already_pressed:
+        print("Button clicked!")
+        # os.system("python3 your_script.py")
+        button_already_pressed = True
+
+    elif GPIO.input(BUTTON_PIN) == GPIO.HIGH:
+        # reset when toggle is switched back off
+        button_already_pressed = False
+
+GPIO.add_event_detect(BUTTON_PIN, GPIO.BOTH, callback=button_pressed, bouncetime=300)
+
+print("Ready. Toggle the button.")
 
 try:
     while True:
-        if GPIO.input(BUTTON_PIN) == GPIO.LOW:  # button pressed
-            print("Button pressed!")
-            # os.system("python3 your_script.py")  # run your script
-            time.sleep(1)  # prevent multiple triggers
-        time.sleep(0.1)
+        time.sleep(1)
 
 except KeyboardInterrupt:
     GPIO.cleanup()
-
